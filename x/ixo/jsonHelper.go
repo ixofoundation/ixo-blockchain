4a657f19b02b1183d9dce830b4b234b7220071c9
Cedric Franz <cedric.franz@gmail.com>
2018-05-14 13:33:58 +0200

    Added JSON and Sovrin helpers

--- x/ixo/jsonHelper.go
+++ x/ixo/jsonHelper.go
@@ -6,22 +6,22 @@ import (
 )
 
 type JsonString struct {
-	value string
+	Value string
 }
 
 type JsonObject struct {
-	value map[string]interface{}
+	Value map[string]interface{}
 }
 
 func (jo *JsonObject) String() string {
-	output, err := json.MarshalIndent(jo.value, "", "  ")
+	output, err := json.MarshalIndent(jo.Value, "", "  ")
 	if err != nil {
 		panic(err)
 	}
 	return fmt.Sprintf("%v", string(output))
 }
 func (js *JsonString) ParseJSON() JsonObject {
-	jsonBytes := []byte(js.value)
+	jsonBytes := []byte(js.Value)
 	var f interface{}
 	err := json.Unmarshal(jsonBytes, &f)
 	if err != nil {
