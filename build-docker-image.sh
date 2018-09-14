16f500cc93f4fd8f3e6efc71890b66de29dbbbd2
source-code-smith <herman@sourcecodesmith.com>
2018-09-14 15:38:46 +0200

    accepting argument to use as image tag (but default to dev)

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+IMAGE_TAG=$1
+if [ -z "$IMAGE_TAG" ];
+then
+  IMAGE_TAG=dev
+fi
 COMMIT_HASH=$(git rev-parse --short HEAD)
-docker build -t trustlab/ixo-blockchain:dev  --build-arg COMMIT_HASH=$COMMIT_HASH .
+docker build -t trustlab/ixo-blockchain:$IMAGE_TAG  --build-arg COMMIT_HASH=$COMMIT_HASH .
