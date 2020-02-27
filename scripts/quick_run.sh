766f208acadc76bb43efd5513692c7dd318945d1
migueldingli1997 <miguel.dingli.15@um.edu.mt>
2020-02-27 13:12:19 +0100

    Fixed scripts.

--- scripts/quick_run.sh
+++ scripts/quick_run.sh
@@ -2,7 +2,7 @@
 
 PASSWORD="12345678"
 
-ixod init local --chain-id pandora-test-1
+ixod init local --chain-id pandora-1
 
 yes $PASSWORD | ixocli keys delete miguel --force
 yes $PASSWORD | ixocli keys delete francesco --force
@@ -18,7 +18,7 @@ yes $PASSWORD | ixod add-genesis-account "$(ixocli keys show miguel -a)" 1000000
 yes $PASSWORD | ixod add-genesis-account "$(ixocli keys show francesco -a)" 100000000stake,1000000res,1000000rez
 yes $PASSWORD | ixod add-genesis-account "$(ixocli keys show shaun -a)" 100000000stake,1000000res,1000000rez
 
-ixocli config chain-id bondschain-1
+ixocli config chain-id pandora-1
 ixocli config output json
 ixocli config indent true
 ixocli config trust-node true
@@ -29,4 +29,4 @@ ixod collect-gentxs
 ixod validate-genesis
 
 ixod start --pruning "syncable" &
-ixocli rest-server --chain-id pandora-test-1 --trust-node && fg
+ixocli rest-server --chain-id pandora-1 --trust-node && fg
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 PASSWORD="12345678"
 
-ixod init local --chain-id pandora-test-1
+ixod init local --chain-id pandora-1
 
 yes $PASSWORD | ixocli keys delete miguel --force
 yes $PASSWORD | ixocli keys delete francesco --force
@@ -26,7 +26,7 @@ yes $PASSWORD | ixod add-genesis-account "$(ixocli keys show miguel -a)" 1000000
 yes $PASSWORD | ixod add-genesis-account "$(ixocli keys show francesco -a)" 100000000stake,1000000res,1000000rez
 yes $PASSWORD | ixod add-genesis-account "$(ixocli keys show shaun -a)" 100000000stake,1000000res,1000000rez
 
-ixocli config chain-id bondschain-1
+ixocli config chain-id pandora-1
 ixocli config output json
 ixocli config indent true
 ixocli config trust-node true
@@ -36,5 +36,5 @@ yes $PASSWORD | ixod gentx --name miguel
 ixod collect-gentxs
 ixod validate-genesis
 
-#ixod start --pruning "syncable" &
-#ixocli rest-server --chain-id pandora-test-1 --trust-node && fg
+ixod start --pruning "syncable" &
+ixocli rest-server --chain-id pandora-1 --trust-node && fg
