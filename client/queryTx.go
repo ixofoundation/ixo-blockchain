f6d2eb52256c081cb824149e1d6cd2d6fe81f250
Miguel <miguel.dingli.15@um.edu.mt>
2020-03-25 17:34:12 +0100

    Enabled querying of transactions by events.

--- client/queryTx.go
+++ client/queryTx.go
@@ -4,10 +4,12 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
-	
+
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -17,56 +19,58 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	coreTypes "github.com/tendermint/tendermint/rpc/core/types"
-	
+
+	genutilrest "github.com/cosmos/cosmos-sdk/x/genutil/client/rest"
 	"github.com/ixofoundation/ixo-cosmos/x/ixo"
 )
 
 func QueryTxCmd(cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
-		
+
 		Use:   "tx [hash]",
 		Short: "Query for a transaction by hash in a committed block",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
-			
+
 			output, err := QueryTx(cliCtx, args[0])
 			if err != nil {
 				return err
 			}
-			
+
 			if output.Empty() {
 				return fmt.Errorf("No transaction found with hash %s", args[0])
 			}
-			
+
 			return cliCtx.PrintOutput(output)
 		},
 	}
-	
+
 	cmd.Flags().StringP(flags.FlagNode, "n", "tcp://localhost:26657", "Node to connect to")
 	_ = viper.BindPFlag(flags.FlagNode, cmd.Flags().Lookup(flags.FlagNode))
 	cmd.Flags().Bool(flags.FlagTrustNode, false, "Trust connected full node (don't verify proofs for responses)")
 	_ = viper.BindPFlag(flags.FlagTrustNode, cmd.Flags().Lookup(flags.FlagTrustNode))
-	
+
 	return cmd
 }
 
 func RegisterQueryTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	r.HandleFunc("/txs/{hash}", QueryTxRequestHandlerFn(cliCtx)).Methods("GET")
+	r.HandleFunc("/txs", QueryTxsRequestHandlerFn(cliCtx)).Methods("GET")
 }
 
 func QueryTxRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		
+
 		w.Header().Set("Content-Type", "application/json")
 		vars := mux.Vars(r)
 		hashHexStr := vars["hash"]
-		
+
 		cliCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, cliCtx, r)
 		if !ok {
 			return
 		}
-		
+
 		output, err := QueryTx(cliCtx, hashHexStr)
 		if err != nil {
 			if strings.Contains(err.Error(), hashHexStr) {
@@ -76,17 +80,67 @@ func QueryTxRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		
+
 		if output.Empty() {
 			rest.WriteErrorResponse(w, http.StatusNotFound, fmt.Sprintf("no transaction found with hash %s", hashHexStr))
 		}
-		
+
 		data, err := json.Marshal(output)
 		if err != nil {
 			rest.WriteErrorResponse(w, http.StatusInternalServerError, fmt.Sprintf("parse error,%s", err.Error()))
 		}
-		
+
 		_, _ = w.Write(data)
+	}
+}
+
+func QueryTxsRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest,
+				sdk.AppendMsgToErr("could not parse query parameters", err.Error()))
+			return
+		}
+
+		// if the height query param is set to zero, query for genesis transactions
+		heightStr := r.FormValue("height")
+		if heightStr != "" {
+			if height, err := strconv.ParseInt(heightStr, 10, 64); err == nil && height == 0 {
+				genutilrest.QueryGenesisTxs(cliCtx, w)
+				return
+			}
+		}
+
+		var (
+			events      []string
+			txs         []sdk.TxResponse
+			page, limit int
+		)
+
+		cliCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, cliCtx, r)
+		if !ok {
+			return
+		}
+
+		if len(r.Form) == 0 {
+			rest.PostProcessResponseBare(w, cliCtx, txs)
+			return
+		}
+
+		events, page, limit, err = rest.ParseHTTPArgs(r)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		searchResult, err := utils.QueryTxsByEvents(cliCtx, events, page, limit)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		rest.PostProcessResponseBare(w, cliCtx, searchResult)
 	}
 }
 
@@ -95,33 +149,33 @@ func QueryTx(cliCtx context.CLIContext, hashHexStr string) (sdk.TxResponse, erro
 	if err != nil {
 		return sdk.TxResponse{}, err
 	}
-	
+
 	node, err := cliCtx.GetNode()
 	if err != nil {
 		return sdk.TxResponse{}, err
 	}
-	
+
 	resTx, err := node.Tx(hash, !cliCtx.TrustNode)
 	if err != nil {
 		return sdk.TxResponse{}, err
 	}
-	
+
 	if !cliCtx.TrustNode {
 		if err = ValidateTxResult(cliCtx, resTx); err != nil {
 			return sdk.TxResponse{}, err
 		}
 	}
-	
+
 	resBlocks, err := getBlocksForTxResults(cliCtx, []*coreTypes.ResultTx{resTx})
 	if err != nil {
 		return sdk.TxResponse{}, err
 	}
-	
+
 	out, err := formatTxResult(cliCtx.Codec, resTx, resBlocks[resTx.Height])
 	if err != nil {
 		return out, err
 	}
-	
+
 	return out, nil
 }
 
@@ -130,7 +184,7 @@ func getBlocksForTxResults(cliCtx context.CLIContext, resTxs []*coreTypes.Result
 	if err != nil {
 		return nil, err
 	}
-	
+
 	resBlocks := make(map[int64]*coreTypes.ResultBlock)
 	for _, resTx := range resTxs {
 		if _, ok := resBlocks[resTx.Height]; !ok {
@@ -138,11 +192,11 @@ func getBlocksForTxResults(cliCtx context.CLIContext, resTxs []*coreTypes.Result
 			if err != nil {
 				return nil, err
 			}
-			
+
 			resBlocks[resTx.Height] = resBlock
 		}
 	}
-	
+
 	return resBlocks, nil
 }
 
@@ -151,7 +205,7 @@ func formatTxResult(cdc *codec.Codec, resTx *coreTypes.ResultTx, resBlock *coreT
 	if err != nil {
 		return sdk.TxResponse{}, err
 	}
-	
+
 	return sdk.NewResponseResultTx(resTx, tx, resBlock.Block.Time.Format(time.RFC3339)), nil
 }
 
@@ -165,12 +219,12 @@ func ValidateTxResult(cliCtx context.CLIContext, resTx *coreTypes.ResultTx) erro
 		if err != nil {
 			return err
 		}
-		
+
 		err = resTx.Proof.Validate(check.Header.DataHash)
 		if err != nil {
 			return err
 		}
 	}
-	
+
 	return nil
 }
