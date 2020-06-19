e847c597c529be562e5e2b538a0d30bb367d74ef
migueldingli1997 <migueldingli97@gmail.com>
2020-06-19 18:59:50 +0200

    Reverted minor change.

--- scripts/run_with_all_data.sh
+++ scripts/run_with_all_data.sh
@@ -71,9 +71,9 @@ ixod collect-gentxs
 ixod validate-genesis
 
 # Uncomment the below to broadcast node RPC endpoint
-FROM="laddr = \"tcp:\/\/127.0.0.1:26657\""
-TO="laddr = \"tcp:\/\/0.0.0.0:26657\""
-sed -i "s/$FROM/$TO/" "$HOME"/.ixod/config/config.toml
+#FROM="laddr = \"tcp:\/\/127.0.0.1:26657\""
+#TO="laddr = \"tcp:\/\/0.0.0.0:26657\""
+#sed -i "s/$FROM/$TO/" "$HOME"/.ixod/config/config.toml
 
 ixod start --pruning "syncable" &
 ixocli rest-server --chain-id pandora-1 --trust-node && fg
