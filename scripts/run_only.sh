7be531fa3f182ecf0ceb19ebb0718fd8297febdb
migueldingli1997 <migueldingli97@gmail.com>
2020-07-09 13:46:15 +0200

    Added more options in runner scripts.

--- scripts/run_only.sh
+++ scripts/run_only.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Uncomment the below to broadcast REST endpoint
+# Do not forget to comment the bottom lines !!
+#ixod start --pruning "syncable" &
+#ixocli rest-server --chain-id pandora-1 --laddr="tcp://0.0.0.0:1317" --trust-node && fg
+
 ixod start --pruning "syncable" &
 ixocli rest-server --chain-id pandora-1 --trust-node && fg
