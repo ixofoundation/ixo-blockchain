02a907e7b68dc3f22b6b55d96738db5dc93f503b
migueldingli1997 <migueldingli97@gmail.com>
2021-01-27 16:44:05 +0100

    Added set_next_alpha REST tx.

--- x/bonds/client/rest/tx.go
+++ x/bonds/client/rest/tx.go
@@ -16,6 +16,7 @@ import (
 func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	r.HandleFunc("/bonds/create_bond", createBondRequestHandler(cliCtx)).Methods("POST")
 	r.HandleFunc("/bonds/edit_bond", editBondRequestHandler(cliCtx)).Methods("POST")
+	r.HandleFunc("/bonds/set_next_alpha", setNextAlphaRequestHandler(cliCtx)).Methods("POST")
 	r.HandleFunc("/bonds/buy", buyRequestHandler(cliCtx)).Methods("POST")
 	r.HandleFunc("/bonds/sell", sellRequestHandler(cliCtx)).Methods("POST")
 	r.HandleFunc("/bonds/swap", swapRequestHandler(cliCtx)).Methods("POST")
@@ -194,6 +195,43 @@ func editBondRequestHandler(cliCtx context.CLIContext) http.HandlerFunc {
 		msg := types.NewMsgEditBond(req.Token, req.Name, req.Description,
 			req.OrderQuantityLimits, req.SanityRate,
 			req.SanityMarginPercentage, req.EditorDid, req.BondDid)
+		if err := msg.ValidateBasic(); err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
+	}
+}
+
+type setNextAlphaReq struct {
+	BaseReq   rest.BaseReq `json:"base_req" yaml:"base_req"`
+	Token     string       `json:"token" yaml:"token"`
+	NewAlpha  string       `json:"new_alpha" yaml:"new_alpha"`
+	BondDid   string       `json:"bond_did" yaml:"bond_did"`
+	EditorDid string       `json:"editor_did" yaml:"editor_did"`
+}
+
+func setNextAlphaRequestHandler(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req setNextAlphaReq
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			return
+		}
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+
+		// Parse new alpha
+		newAlpha, err := sdk.NewDecFromStr(req.NewAlpha)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		msg := types.NewMsgSetNextAlpha(req.Token, newAlpha, req.EditorDid, req.BondDid)
 		if err := msg.ValidateBasic(); err != nil {
 			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
