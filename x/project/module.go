b1b3407bc3fe94ba73d4c2ec2a0f59c757a39f7c
Stef <stefania.damato.15@um.edu.mt>
2021-05-12 10:31:58 +0100

    project: added comments

--- x/project/module.go
+++ x/project/module.go
@@ -23,25 +23,31 @@ import (
 	"github.com/ixofoundation/ixo-blockchain/x/project/keeper"
 )
 
+// Type check to ensure the interface is properly implemented
 var (
 	_ module.AppModule      = AppModule{}
 	_ module.AppModuleBasic = AppModuleBasic{}
 )
 
+// AppModuleBasic defines the basic application module used by the project module.
 type AppModuleBasic struct{}
 
+// Name returns the project module's name.
 func (AppModuleBasic) Name() string {
 	return types.ModuleName
 }
 
+// RegisterLegacyAminoCodec registers the project module's types for the given codec.
 func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	types.RegisterLegacyAminoCodec(cdc)
 }
 
+// DefaultGenesis returns default genesis state as raw bytes for the project module.
 func (AppModuleBasic) DefaultGenesis(cdc codec.JSONMarshaler) json.RawMessage {
 	return cdc.MustMarshalJSON(types.DefaultGenesisState())
 }
 
+// ValidateGenesis performs genesis state validation for the project module.
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONMarshaler, config client.TxEncodingConfig, bz json.RawMessage) error {
 	var data types.GenesisState
 	err := cdc.UnmarshalJSON(bz, &data)
@@ -51,28 +57,33 @@ func (AppModuleBasic) ValidateGenesis(cdc codec.JSONMarshaler, config client.TxE
 	return types.ValidateGenesis(data)
 }
 
+// RegisterRESTRoutes registers the REST routes for the project module.
 func (AppModuleBasic) RegisterRESTRoutes(ctx client.Context, rtr *mux.Router) {
 	rest.RegisterHandlers(ctx, rtr)
 }
 
+// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the project module.
 func (a AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
 	_ = types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
 }
 
+// GetTxCmd returns the root tx command for the project module.
 func (AppModuleBasic) GetTxCmd() *cobra.Command {
 	return cli.NewTxCmd()
 }
 
+// GetQueryCmd returns the root query command for the project module.
 func (AppModuleBasic) GetQueryCmd() *cobra.Command {
 	return cli.GetQueryCmd()
 }
-
+// RegisterInterfaces registers interfaces and implementations of the project module.
 func (a AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	types.RegisterInterfaces(registry)
 }
 
 //____________________________________________________________________________
 
+// AppModule implements an application module for the project module.
 type AppModule struct {
 	AppModuleBasic
 	keeper         keeper.Keeper
@@ -80,6 +91,7 @@ type AppModule struct {
 	bankKeeper     bankkeeper.Keeper
 }
 
+// NewAppModule creates a new AppModule object
 func NewAppModule(keeper keeper.Keeper, paymentsKeeper payments.Keeper,
 	bankKeeper bankkeeper.Keeper) AppModule {
 
@@ -91,29 +103,37 @@ func NewAppModule(keeper keeper.Keeper, paymentsKeeper payments.Keeper,
 	}
 }
 
+// Name returns the project module's name.
 func (AppModule) Name() string {
 	return types.ModuleName
 }
 
+// RegisterInvariants registers module invariants.
 func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {}
 
+// Route returns the message routing key for the project module.
 func (am AppModule) Route() sdk.Route {
 	return sdk.NewRoute(types.RouterKey, NewHandler(am.keeper, am.paymentsKeeper, am.bankKeeper))
 }
 
+// QuerierRoute returns the project module's querier route name.
 func (AppModule) QuerierRoute() string {
 	return types.QuerierRoute
 }
 
+// LegacyQuerierHandler returns the project module sdk.Querier.
 func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
 	return keeper.NewQuerier(am.keeper, legacyQuerierCdc)
 }
 
+// RegisterServices registers module services.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper, am.bankKeeper, am.paymentsKeeper))
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 }
 
+// InitGenesis performs genesis initialization for the project module. It returns
+// no validator updates.
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONMarshaler, data json.RawMessage) []abci.ValidatorUpdate {
 	var genesisState types.GenesisState
 	cdc.MustUnmarshalJSON(data, &genesisState)
@@ -121,14 +141,17 @@ func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONMarshaler, data j
 	return []abci.ValidatorUpdate{}
 }
 
+// ExportGenesis returns the exported genesis state as raw bytes for the project module.
 func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONMarshaler) json.RawMessage {
 	gs := ExportGenesis(ctx, am.keeper)
 	return cdc.MustMarshalJSON(gs)
 }
 
+// BeginBlock returns the begin blocker for the project module. In this case it performs a no-op.
 func (am AppModule) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) {
 }
 
+// EndBlock returns the end blocker for the project module. It returns no validator updates.
 func (AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
 	return []abci.ValidatorUpdate{}
 }
