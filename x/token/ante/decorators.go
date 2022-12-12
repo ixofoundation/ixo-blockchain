af1297d9d90ea6e8c70343e33ddcf693bfc60df7
Jacob Gadikian <jacobgadikian@gmail.com>
2022-12-12 15:35:43 +0700

    comment out issues in decorators.go

--- x/token/ante/decorators.go
+++ x/token/ante/decorators.go
@@ -3,7 +3,6 @@ package ante
 import (
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	tokenkeeper "github.com/ixofoundation/ixo-blockchain/x/token/keeper"
 	tokentypes "github.com/ixofoundation/ixo-blockchain/x/token/types"
 )
@@ -21,7 +20,8 @@ func NewBlockNftContractTransferForTokenDecorator(tokenKeeper tokenkeeper.Keeper
 func (sud BlockNftContractTransferForTokenDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
 
 	for _, msg := range tx.GetMsgs() {
-		wasmMsg, ok := msg.(*wasmtypes.MsgExecuteContract)
+		// TODO: change the blank to wasmMag later
+		_, ok := msg.(*wasmtypes.MsgExecuteContract)
 		if !ok {
 			continue
 		}
@@ -29,9 +29,9 @@ func (sud BlockNftContractTransferForTokenDecorator) AnteHandle(ctx sdk.Context,
 		var params tokentypes.Params
 		sud.tokenKeeper.ParamSpace.GetParamSetIfExists(ctx, &params)
 
-		if wasmMsg.Contract == params.NftContractAddress {
-			return ctx, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Cant execute contract set as the nft contract address")
-		}
+		//		if wasmMsg.Contract == params.NftContractAddress {
+		//			return ctx, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Cant execute contract set as the nft contract address")
+		//		}
 	}
 
 	return next(newCtx, tx, simulate)
