c4be3cb74f146d0f7e32d6c53816690f2f8b0c98
Michael-Ixo <113350374+Michael-Ixo@users.noreply.github.com>
2023-07-03 12:17:14 +0200

    Develop (#304)
    * Redo from develop branch as base (#295)
    
    * Redo from develop branch as base (#295) (#296)
    
    * Update README.md
    
    JAMBO repo name change
    
    * Update README.md
    
    Developers docs link updated
    
    * Update README.md
    
    JAMBO repo name change
    
    * Update README.md
    
    Developers docs link updated
    
    * chore: update readme
    
    * fix(wasm): enable wasm statesync
    
    ---------
    
    Co-authored-by: Alwyn van Wyk <alwyn.vanwyk@ixo.world>

--- app/app.go
+++ app/app.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/CosmWasm/wasmd/x/wasm"
 	wasmclient "github.com/CosmWasm/wasmd/x/wasm/client"
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
@@ -680,6 +681,15 @@ func NewIxoApp(
 	app.SetBeginBlocker(app.BeginBlocker)
 	app.SetAnteHandler(ixoAnteHandler)
 	app.SetEndBlocker(app.EndBlocker)
+
+	if manager := app.SnapshotManager(); manager != nil {
+		err = manager.RegisterExtensions(
+			wasmkeeper.NewWasmSnapshotter(app.CommitMultiStore(), &app.WasmKeeper),
+		)
+		if err != nil {
+			panic("failed to register snapshot extension: " + err.Error())
+		}
+	}
 
 	if loadLatest {
 		if err := app.LoadLatestVersion(); err != nil {
