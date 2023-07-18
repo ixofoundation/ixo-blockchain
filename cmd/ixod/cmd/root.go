6c67282743f433effe22cbf0603db8941462efe5
Michael Pretorius <michael@ixo.world>
2023-07-18 14:16:09 +0200

    chore: small updates

--- cmd/ixod/cmd/root.go
+++ cmd/ixod/cmd/root.go
@@ -48,7 +48,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 	config.Seal()
 
 	initClientCtx := client.Context{}.
-		WithJSONCodec(encodingConfig.Marshaler).
+		WithCodec(encodingConfig.Marshaler).
 		WithInterfaceRegistry(encodingConfig.InterfaceRegistry).
 		WithTxConfig(encodingConfig.TxConfig).
 		WithLegacyAmino(encodingConfig.Amino).
